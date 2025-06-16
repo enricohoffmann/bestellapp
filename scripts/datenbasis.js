9b8eb6cfc73a62eaa38152a763ca61eb5d52d1da
Enrico Hoffmann <enricohoffmann@web.de>
2025-06-16 20:13:11 +0200

    feat: Bilder hinzugefügt, erste Datenbasis erstellt

---
+++ scripts/datenbasis.js
@@ -0,0 +1,92 @@
+let myMainDishes = [
+    {
+        'id': 1000,
+        'name': 'Teriyaki-Burger',
+        'description': 'Burger mit Hähnchenbrustfilet, Wasabi-Paste und Cheddar einen japanischen Touch!',
+        'price': 14.98
+    },
+    {
+        'id': 1001,
+        'name': 'Grillgemüse­-Burger',
+        'description': 'Zwischen Burger-Brötchen treffen frische Zucchini, Paprikaschoten und Rauke aufeinander.',
+        'price': 12.99
+    },
+    {
+        'id': 1002,
+        'name': 'Lachs-Burger',
+        'description': 'Unter der Lachsbulette liegen Rote Bete und Avocado-Creme, darüber Wachtelspiegelei, Bacon und Meerrettich!',
+        'price': 17.98
+    },
+    {
+        'id': 1003,
+        'name': 'Porchetta-Burger',
+        'description': 'Dieser Burger ist bestens belegt, und zwar mit Schalotten, Tomaten, Basilikum und Porchetta!',
+        'price': 10.80
+    },
+    {
+        'id': 1004,
+        'name': 'Steak-Burger',
+        'description': 'Diese Kombination ist einfach fantastisch: Die Roastbeef-Scheiben harmonieren perfekt zu Krautsalat und Zwiebelringen!',
+        'price': 18.99
+    }
+
+];
+
+let mySideDishes = [
+    {
+        'id': 2000,
+        'name': 'Pommes Frittes',
+        'description': 'Handgemachte Pommes asu Kartoffeln der Region.',
+        'price': 6.90
+    },
+    {
+        'id': 2001,
+        'name': 'Gebratene Zwiebelringe',
+        'description': 'Zwiebelringe mit leckerer Panade gebraten.',
+        'price': 4.99
+    },
+    {
+        'id': 2002,
+        'name': 'Gemischter Salat',
+        'description': 'Saisonales zusammenstellung von regionalen Salaten.',
+        'price': 5.20
+    },
+    {
+        'id': 2003,
+        'name': 'Krautsalat',
+        'description': 'Zubereitet nach Art des Hauses mit Kümmel.',
+        'price': 4.98
+    },
+    {
+        'id': 2004,
+        'name': 'Tomatensalat',
+        'description': 'Tomatensalat mit Zwiebeln und Mozarella. Nur während der Saison',
+        'price': 7.30
+    }
+];
+
+
+//Beilagen dürfen nur zu Hauptgerichten bestellt werden, max. 2 Beilagen pro Hauptgericht
+
+let myShoppingCard = {
+    'mainDishes':[],
+    'sideDishes':[],
+    'getCountMainDishes':function () {
+        return this.mainDishes.length;
+    },
+    'getCountSideDishes':function () {
+        return this.sideDishes.length;
+    },
+    'addDish':function(category, dish) {
+        //prüfen, ob das Gericht bereits vorhanden ist
+
+        //Ja -> nur die Anzahl erhöhen
+    },
+    'removeDish':function(category, dish) {
+        //prüfen, ob das Gericht bereits vorhanden ist
+
+        //Ja -> nur die Anzahl verringern
+
+        //Wenn das Gericht < 1 ist aus Liste entfernen
+    }
+}
